7b9be1d87fb6c671d20810579a405084d5d384e5
Michal Löffler <michal.loffler@vacuumlabs.com>
2023-02-09 13:43:32 +0100

    improve request validation

--- app/Http/Requests/CreateSalesmanRequest.php
+++ app/Http/Requests/CreateSalesmanRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class CreateSalesmanRequest extends FormRequest
 {
@@ -29,22 +30,27 @@ class CreateSalesmanRequest extends FormRequest
             'first_name' => ['required', 'min:2', 'max:50'],
             'last_name' => ['required', 'min:2', 'max:50'],
             'prosight_id' => ['required', 'digits:5'],
-            'email' => ['required', 'email:rfc,dns'],
-            'gender' => ['required'],
-            'titles_before' => ['array'],
-            'titles_before.*' => ['string', 'distinct', 'min:3'],
-            'titles_after' => ['array'],
-            'titles_after.*' => ['string', 'distinct', 'min:3'],
+            'email' => ['unique:salesmen', 'required', 'email:rfc,dns'],
+            'gender' => ['required', Rule::in('m', 'f')],
+            'titles_before' => ['array', 'max:10'],
+            'titles_before.*' => ['distinct', Rule::in(['Bc.', 'Mgr.', 'Ing.', 'JUDr.', 'MVDr.', 'MUDr.', 'PaedDr.', 'prof.', 'doc.', 'dipl.', 'MDDr.', 'Dr.', 'Mgr. art.', 'ThLic.', 'PhDr.', 'PhMr.', 'RNDr.', 'ThDr.', 'RSDr.', 'arch.', 'PharmDr.' ])],
+            'titles_after' => ['array', 'max:10'],
+            'titles_after.*' => ['distinct', Rule::in(['CSc.', 'DrSc.', 'PhD.', 'ArtD.', 'DiS', 'DiS.art', 'FEBO', 'MPH', 'BSBA', 'MBA', 'DBA', 'MHA', 'FCCA', 'MSc.', 'FEBU', 'LL.M'])],
+            'marital_status' => [Rule::in(['single', 'married', 'divorced', 'widowed'])],
+            'phone' => ['string']
         ];
     }
 
     public function failedValidation(Validator $validator)
     {
         throw new HttpResponseException(response()->json([
-            'success' => false,
-            'message'   => 'Validation error',
-            'data'      => $validator->errors()
-        ]));
+            'errors' => array_map(function ($message) {
+                return [
+                    'code' => 'VALIDATION_ERROR', // TODO: set specific validation error code
+                    'message' => $message
+                ];
+            }, $validator->errors()->all()),
+        ], 400));
     }
 
     public function passedValidation()
